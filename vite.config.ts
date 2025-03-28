ad219bdcd5c0d3b11deb3bb62baac91ca01750d0
Edy Segura <edysegura@gmail.com>
2025-03-28 16:56:19 -0300

    feat: add PWA support with VitePWA plugin and configure manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,29 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   base: "/html5-base64",
+  plugins: [
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "HTML5 Base64",
+        short_name: "Base64",
+        description: "HTML5 Base64 Encoder/Decoder",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "vite.svg",
+            sizes: "192x192",
+            type: "image/svg+xml",
+          },
+          {
+            src: "vite.svg",
+            sizes: "512x512",
+            type: "image/svg+xml",
+          },
+        ],
+      },
+    }),
+  ],
 });
